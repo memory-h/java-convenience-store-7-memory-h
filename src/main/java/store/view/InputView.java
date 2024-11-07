c4c9fab7f82b5d7da834e3d219c3932380cabb3c
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-07 18:10:54 +0900

    refactor(InputView): 값의 변경을 방지하기 위해 final 키워드 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -4,25 +4,25 @@ public class InputView {
 
     private static final String DELIMITER_REGEX = "^\\[[^\\]]+\\](,\\[[^\\]]+\\])*$";
 
-    public static String readProducts(String userInput) {
+    public static String readProducts(final String userInput) {
         Validator.validateDelimiter(userInput);
         return userInput;
     }
 
-    public static String readUserInput(String userInput) {
+    public static String readUserInput(final String userInput) {
         Validator.validateUserInputIsNotEmpty(userInput);
         return userInput;
     }
 
     private static class Validator {
 
-        private static void validateUserInputIsNotEmpty(String userInput) {
+        private static void validateUserInputIsNotEmpty(final String userInput) {
             if (userInput == null || userInput.isBlank()) {
                 throw new IllegalArgumentException("[ERROR] 상품명과 수량을 비어있을 수 없습니다.");
             }
         }
 
-        private static void validateDelimiter(String userInput) {
+        private static void validateDelimiter(final String userInput) {
             if (!userInput.matches(DELIMITER_REGEX)) {
                 throw new IllegalArgumentException("[ERROR] 개별 상품은 대괄호와 쉼표로 구분되어야 합니다.");
             }
