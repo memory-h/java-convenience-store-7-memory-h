7fc3c222284f4b8b596265c2d986544f6c508ca1
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-07 17:54:56 +0900

    test(InputViewTest): 상품명과 수량이 비어있으면 예외가 발생하는 테스트 작성

--- src/test/java/store/view/InputViewTest.java
+++ src/test/java/store/view/InputViewTest.java
@@ -2,6 +2,7 @@ package store.view;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -20,6 +21,16 @@ public class InputViewTest {
         assertThatThrownBy(() -> InputView.readProducts(condition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 개별 상품은 대괄호와 쉼표로 구분되어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"\n", "\t"})
+    @DisplayName("상품명과 수량이 비어있으면 예외가 발생한다.")
+    void 상품명과_수량이_비어있으면_예외_발생(String condition) {
+        assertThatThrownBy(() -> InputView.readUserInput(condition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 상품명과 수량을 비어있을 수 없습니다.");
     }
 
 }
