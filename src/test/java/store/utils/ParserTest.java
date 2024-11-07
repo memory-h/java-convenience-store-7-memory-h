608bda07739a8a922ad01462a44ab47c383a3372
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-07 18:32:04 +0900

    test(ParserTest): 상품명과 수량이 하이픈으로 구분하는 테스트 작성

--- src/test/java/store/utils/ParserTest.java
+++ src/test/java/store/utils/ParserTest.java
@@ -2,6 +2,11 @@ package store.utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import store.view.InputView;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +21,23 @@ public class ParserTest {
 
         assertThat(splitUserInput).hasSize(2);
         assertThat(splitUserInput).contains("[콜라-3]", "[에너지바-5]");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "[콜라-3],[에너지바-5],[사이다-6]",
+            "[콜라-3],[탄산수-2],[초코바-1],[오렌지주스-10]"
+    })
+    @DisplayName("상품명과 수량을 하이픈으로 구분되는지 테스트한다.")
+    void 상품명과_수량은_하이픈으로_구분한다(String condition) {
+        String validateDelimiter = InputView.readProducts(condition);
+        String[] splitProducts = Parser.splitByDelimiter(validateDelimiter);
+        Map<String, String> products = Parser.splitByHyphen(splitProducts);
+
+        assertThat(products).hasSize(products.size());
+        products.keySet()
+                .forEach(productName -> assertThat(products.get(productName))
+                        .isEqualTo(products.get(productName)));
     }
 
 }
