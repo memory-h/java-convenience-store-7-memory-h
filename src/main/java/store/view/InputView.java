e47700c2c78048a841c2f265aa66b8adcc949cc0
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-07 21:18:59 +0900

    refactor(InputView): InputView 클래스의 입력 방식을 Console.readLine() 으로 수정

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,16 +1,19 @@
 package store.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import store.validator.InputViewValidator;
 import store.validator.ParserValidator;
 
 public class InputView {
 
-    public static String readProducts(final String userInput) {
+    public static String readProducts() {
+        String userInput = readUserInput();
         ParserValidator.validateIndividualProductDelimiter(userInput);
         return userInput;
     }
 
-    public static String readUserInput(final String userInput) {
+    public static String readUserInput() {
+        String userInput = Console.readLine();
         InputViewValidator.validateUserInputIsNotEmpty(userInput);
         return userInput;
     }
