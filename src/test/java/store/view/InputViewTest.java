8cb19a6ee9b979287c4c45010067617c4aace29e
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-07 17:11:54 +0900

    test(InputViewTest): 개별 상품 입력 조건이 다르면 예외가 발생하는 테스트 작성

---
+++ src/test/java/store/view/InputViewTest.java
@@ -0,0 +1,25 @@
+package store.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputViewTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "[콜라-3];[에너지바-5];[사이다-6]",
+            "[콜라-3].[에너지바-5].[사이다-6]",
+            "[콜라-3]:[에너지바-5]:[사이다-6]",
+            "[콜라-3],[에너지바-5]:[사이다-6]"
+    })
+    @DisplayName("개별 상품이 대괄호와 쉼표로 구분되지 않으면 예외가 발생한다.")
+    void 개별_상품이_대괄호와_쉼표로_구분되지_않으면_예외_발생(String condition) {
+        assertThatThrownBy(() -> InputView.readProducts(condition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 개별 상품은 대괄호와 쉼표로 구분되어야 합니다.");
+    }
+
+}
