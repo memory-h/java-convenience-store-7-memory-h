a2862c1e6ab2a18d2f1d23e96a317579870187c0
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-07 18:34:35 +0900

    feat(Parser): 상품명과 수량이 하이픈으로 구분하는 로직 추가

--- src/main/java/store/utils/Parser.java
+++ src/main/java/store/utils/Parser.java
@@ -1,11 +1,28 @@
 package store.utils;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Parser {
 
     private static final String COMMA_DELIMITER = ",";
+    private static final String HYPHEN_DELIMITER = "-";
+    private static final int PRODUCT_NAME_INDEX = 0;
+    private static final int PRODUCT_QUANTITY_INDEX = 1;
 
     public static String[] splitByDelimiter(final String userInput) {
         return userInput.split(COMMA_DELIMITER);
+    }
+
+    public static Map<String, String> splitByHyphen(final String[] splitProducts) {
+        final Map<String, String> products = new HashMap<>();
+        Arrays.stream(splitProducts)
+                .forEach(product -> {
+                    String[] productNameAndQuantity = product.split(HYPHEN_DELIMITER);
+                    products.put(productNameAndQuantity[PRODUCT_NAME_INDEX], productNameAndQuantity[PRODUCT_QUANTITY_INDEX]);
+                });
+        return products;
     }
 
 }
