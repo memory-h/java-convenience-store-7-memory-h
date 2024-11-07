d4410aec8436055dc479f6f9ca7958b101ddb87c
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-07 21:24:26 +0900

    feat(ParserValidator): 상품명과 수량 사이의 구분자가 하이픈인지 검증하는 로직 추가

--- src/main/java/store/validator/ParserValidator.java
+++ src/main/java/store/validator/ParserValidator.java
@@ -3,10 +3,19 @@ package store.validator;
 public class ParserValidator {
 
     private static final String INDIVIDUAL_PRODUCT_DELIMITER_REGEX = "^\\[[^\\]]+\\](,\\[[^\\]]+\\])*$";
+    private static final String PRODUCT_AND_QUANTITY_DELIMITER_REGEX = "[가-힣a-zA-Z]+-[가-힣a-zA-Z]";
 
     public static void validateIndividualProductDelimiter(final String userInput) {
         if (!userInput.matches(INDIVIDUAL_PRODUCT_DELIMITER_REGEX)) {
             throw new IllegalArgumentException("[ERROR] 개별 상품은 대괄호와 쉼표로 구분되어야 합니다.");
+        }
+    }
+
+    public static void validateProductDelimiter(final String[] splitProducts) {
+        for (String splitProduct : splitProducts) {
+            if (!splitProduct.matches(PRODUCT_AND_QUANTITY_DELIMITER_REGEX)) {
+                throw new IllegalArgumentException("[ERROR] 상품명과 수량은 하이픈으로 구분되어야 합니다.");
+            }
         }
     }
 
