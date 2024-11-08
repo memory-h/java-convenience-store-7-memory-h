c494410b44fbd091b2f2ba1d720e863bbfcb1f4b
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-09 02:32:30 +0900

    feat(ProductsPurchase): 편의점이 보유한 수량보다 많이 입력하면 예외가 발생하는 로직 추가

--- src/main/java/store/domain/ProductsPurchase.java
+++ src/main/java/store/domain/ProductsPurchase.java
@@ -12,7 +12,7 @@ public class ProductsPurchase {
     }
 
     public static ProductsPurchase of(List<Product> products, Map<String, Integer> productsUserInput) {
-        Validator.validateProductName(products, productsUserInput);
+        Validator.validateProductNameAndQuantity(products, productsUserInput);
         return new ProductsPurchase(new LinkedHashMap<>());
     }
 
@@ -21,16 +21,32 @@ public class ProductsPurchase {
     }
 
     private static class Validator {
-        private static void validateProductName(List<Product> products, Map<String, Integer> productsUserInput) {
-            Set<String> productNames = products.stream()
-                    .map(Product::getName)
-                    .collect(Collectors.toSet());
-            productsUserInput.keySet().stream()
-                    .filter(inputName -> !productNames.contains(inputName))
-                    .forEach(inputName -> {
-                        throw new IllegalArgumentException("[ERROR] 편의점에 해당 상품은 존재하지 않습니다.");
-                    });
+
+        private static void validateProductNameAndQuantity(List<Product> products, Map<String, Integer> productsUserInput) {
+            Map<String, Integer> productStockMap = products.stream()
+                    .collect(Collectors.toMap(Product::getName, Product::getQuantity, Integer::sum, LinkedHashMap::new));
+            productsUserInput.forEach(
+                    (inputName, inputQuantity) -> validateProductAvailability(inputName, inputQuantity, productStockMap));
         }
+
+        private static void validateProductAvailability(String inputName, int inputQuantity, Map<String, Integer> productStockMap) {
+            validateProductExists(inputName, productStockMap);
+            validateProductQuantity(inputName, inputQuantity, productStockMap);
+        }
+
+        private static void validateProductExists(String inputName, Map<String, Integer> productStockMap) {
+            if (!productStockMap.containsKey(inputName)) {
+                throw new IllegalArgumentException("[ERROR] 편의점에 해당 상품은 존재하지 않습니다.");
+            }
+        }
+
+        private static void validateProductQuantity(String inputName, int inputQuantity, Map<String, Integer> productStockMap) {
+            int availableQuantity = productStockMap.get(inputName);
+            if (inputQuantity > availableQuantity) {
+                throw new IllegalArgumentException("[ERROR] 편의점이 보유한 수량보다 많이 구매할 수 없습니다.");
+            }
+        }
+
     }
 
 }
