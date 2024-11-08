b7afa8d1e43d159890668adeb5fc0c426222ec42
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-08 18:33:54 +0900

    feat(Products): 상품 목록들을 저장하고 관리하는 Products 생성

---
+++ src/main/java/store/domain/Products.java
@@ -0,0 +1,22 @@
+package store.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Products {
+
+    public final List<Product> products;
+
+    private Products(final List<Product> products) {
+        this.products = products;
+    }
+
+    public static Products from(final List<Product> products) {
+        return new Products(products);
+    }
+
+    public List<Product> getProducts() {
+        return Collections.unmodifiableList(products);
+    }
+
+}
