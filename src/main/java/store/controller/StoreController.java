8ab1e905136fbfa704239eacb5eb29d7d5c8642a
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-08 19:53:51 +0900

    feat(StoreController): storePromotionController의 init() 메서드 추가

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -3,9 +3,11 @@ package store.controller;
 public class StoreController {
 
     private final StoreInventoryController storeInventoryController = new StoreInventoryController();
+    private final StorePromotionController storePromotionController = new StorePromotionController();
 
     public void run() {
         storeInventoryController.init();
+        storePromotionController.init();
     }
 
 }
