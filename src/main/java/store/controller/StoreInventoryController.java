6861a75f5cbc3542b0d2d34161191f7534837949
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-08 18:16:10 +0900

    feat(StoreInventoryController): 상품 목록을 출력 형식으로 가공하는 기능 추가

--- src/main/java/store/controller/StoreInventoryController.java
+++ src/main/java/store/controller/StoreInventoryController.java
@@ -2,6 +2,8 @@ package store.controller;
 
 import store.domain.Product;
 import store.domain.Products;
+import store.utils.PriceFormatter;
+import store.view.OutputView;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -12,14 +14,20 @@ import java.util.List;
 public class StoreInventoryController {
 
     private static final String productFilePath = "src/main/resources/products.md";
+    private static final String CURRENCY_UNIT = "원 ";
+    private static final String QUANTITY_UNIT = "개 ";
+    private static final String BULLET_POINT = "- ";
+    private static final String NEW_LINE = "\n";
     private static final String NULL_STRING = "null";
     private static final String COMMA_DELIMITER = ",";
+    private static final String SPACE = " ";
     private static final int NAME_INDEX = 0;
     private static final int PRICE_INDEX = 1;
     private static final int QUANTITY_INDEX = 2;
     private static final int PROMOTION_INDEX = 3;
 
     private final List<Product> products = new ArrayList<>();
+    private final StringBuilder storeInventoryView = new StringBuilder();
 
     public void init() {
         try(BufferedReader br = new BufferedReader(new FileReader(productFilePath))) {
@@ -29,10 +37,12 @@ public class StoreInventoryController {
             e.printStackTrace();
         }
         Products.from(products);
+        OutputView.startMessage(storeInventoryView.toString());
     }
 
     private void processProductLine(String line) {
         products.add(parseProduct(line));
+        formatProductView(line);
     }
 
     private Product parseProduct(String line) {
@@ -46,6 +56,18 @@ public class StoreInventoryController {
 
     private boolean isPromotion(String[] productList) {
         return !productList[PROMOTION_INDEX].equals(NULL_STRING);
+    }
+
+    private void formatProductView(String line) {
+        String[] productList = line.split(COMMA_DELIMITER);
+        storeInventoryView.append(BULLET_POINT)
+                .append(productList[NAME_INDEX]).append(SPACE)
+                .append(PriceFormatter.format(productList[PRICE_INDEX])).append(CURRENCY_UNIT)
+                .append(productList[QUANTITY_INDEX]).append(QUANTITY_UNIT);
+        if (!productList[PROMOTION_INDEX].equals(NULL_STRING)) {
+            storeInventoryView.append(productList[PROMOTION_INDEX]);
+        }
+        storeInventoryView.append(NEW_LINE);
     }
 
 }
