955e9e4be2c3b3daf591d32903d0a645a69f81ec
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-09 01:02:10 +0900

    refactor(ProductsUserInput): 검증 로직을 ProductsUserInput 내부에서 진행하도록 수정

--- src/main/java/store/domain/ProductsUserInput.java
+++ src/main/java/store/domain/ProductsUserInput.java
@@ -6,10 +6,6 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import static store.validator.ParserValidator.validateIndividualProductDelimiter;
-import static store.validator.ParserValidator.validateProductDelimiter;
-import static store.validator.ProductValidator.validatePositiveQuantity;
-
 public class ProductsUserInput {
 
     private final Map<String, Integer> productsUserInput;
@@ -30,22 +26,50 @@ public class ProductsUserInput {
     }
 
     private static String[] splitUserInputByDelimiter(String userInput) {
-        validateIndividualProductDelimiter(userInput);
+        Validator.validateIndividualProductDelimiter(userInput);
         return Parser.splitByDelimiter(userInput);
     }
 
     private static Map<String, String> parseProductsWithQuantities(String[] splitByDelimiter) {
-        validateProductDelimiter(splitByDelimiter);
+        Validator.validateProductDelimiter(splitByDelimiter);
         return Parser.splitByHyphen(splitByDelimiter);
     }
 
     private static Map<String, Integer> convertToProductQuantityMap(Map<String, String> products) {
         Map<String, Integer> productsUserInput = new LinkedHashMap<>();
         products.keySet().forEach(productName -> {
-            validatePositiveQuantity(products.get(productName));
+            Validator.validatePositiveQuantity(products.get(productName));
             productsUserInput.put(productName, Parser.parseInt(products.get(productName)));
         });
         return productsUserInput;
+    }
+
+    private static class Validator {
+
+        private static final String INDIVIDUAL_PRODUCT_DELIMITER_REGEX = "^\\[[^\\]]+\\](,\\[[^\\]]+\\])*$";
+        private static final String PRODUCT_AND_QUANTITY_DELIMITER_REGEX = "[가-힣a-zA-Z]+-[0-9]+";
+        private static final String POSITIVE_INTEGER_PATTERN = "^[1-9]\\d*$";
+
+        private static void validateIndividualProductDelimiter(final String userInput) {
+            if (!userInput.matches(INDIVIDUAL_PRODUCT_DELIMITER_REGEX)) {
+                throw new IllegalArgumentException("[ERROR] 개별 상품은 대괄호와 쉼표로 구분되어야 합니다.");
+            }
+        }
+
+        private static void validateProductDelimiter(final String[] splitProducts) {
+            for (String splitProduct : splitProducts) {
+                if (!splitProduct.matches(PRODUCT_AND_QUANTITY_DELIMITER_REGEX)) {
+                    throw new IllegalArgumentException("[ERROR] 상품명과 수량은 하이픈으로 구분되어야 합니다.");
+                }
+            }
+        }
+
+        private static void validatePositiveQuantity(final String quantity) {
+            if (!quantity.matches(POSITIVE_INTEGER_PATTERN)) {
+                throw new IllegalArgumentException("[ERROR] 수량은 0보다 큰 정수여야 합니다.");
+            }
+        }
+
     }
 
 }
--- src/test/java/store/domain/ProductsUserInputTest.java
+++ src/test/java/store/domain/ProductsUserInputTest.java
@@ -1,13 +1,13 @@
-package store.validator;
+package store.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import store.utils.Parser;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class ParserValidatorTest {
+class ProductsUserInputTest {
 
     @ParameterizedTest
     @ValueSource(strings = {
@@ -18,24 +18,29 @@ class ParserValidatorTest {
     })
     @DisplayName("개별 상품이 대괄호와 쉼표로 구분되지 않으면 예외가 발생한다.")
     void 개별_상품이_대괄호와_쉼표로_구분되지_않으면_예외_발생(String condition) {
-        assertThatThrownBy(() -> ParserValidator.validateIndividualProductDelimiter(condition))
+        assertThatThrownBy(() -> ProductsUserInput.from(condition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 개별 상품은 대괄호와 쉼표로 구분되어야 합니다.");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {
-            "[콜라-3],[에너지바-5],[사이다-6]",
-            "[콜라-3],[탄산수-2],[초코바-1],[오렌지주스-10]"
+            "[콜라;3],[에너지바-5],[사이다-6]",
+            "[콜라-3],[탄산수:2],[초코바-1],[오렌지주스,10]"
     })
     @DisplayName("상품명과 수량이 하이픈으로 구분되지 않으면 예외가 발생한다.")
     void 상품명과_수량이_하이픈으로_구분되지_않으면_예외_발생(String condition) {
-        InputViewValidator.validateUserInputIsNotEmpty(condition);
-        String[] splitProducts = Parser.splitByDelimiter(condition);
-
-        assertThatThrownBy(() -> ParserValidator.validateProductDelimiter(splitProducts))
+        assertThatThrownBy(() -> ProductsUserInput.from(condition))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 상품명과 수량은 하이픈으로 구분되어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("상품 수량이 0보다 큰 수가 입력되었는지 테스트 한다.")
+    void 수량이_0보다_큰_수인지_테스트() {
+        assertThatThrownBy(() -> ProductsUserInput.from("[사이다-0]"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 수량은 0보다 큰 정수여야 합니다.");
     }
 
 }
