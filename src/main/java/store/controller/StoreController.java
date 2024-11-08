b7e6d23bd219d6e90a64a56abcf5edf45087167c
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-08 23:53:44 +0900

    feat(StoreController): 편의점 상품을 구매하는 로직 추가

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,13 +1,25 @@
 package store.controller;
 
+import store.domain.Products;
+import store.domain.ProductsUserInput;
+import store.domain.Promotions;
+import store.view.InputView;
+
 public class StoreController {
 
     private final StoreInventoryController storeInventoryController = new StoreInventoryController();
     private final StorePromotionController storePromotionController = new StorePromotionController();
 
     public void run() {
-        storeInventoryController.init();
-        storePromotionController.init();
+        final Products products = storeInventoryController.init();
+        final Promotions promotions = storePromotionController.init();
+
+        startPurchaseProcess(products, promotions);
+    }
+
+    private void startPurchaseProcess(Products products, Promotions promotions) {
+        String userInput = InputView.readProducts();
+        ProductsUserInput.from(userInput);
     }
 
 }
