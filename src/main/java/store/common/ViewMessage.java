74b8d5512830cda2d1c3f207877d9eb7abeb8cfe
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-08 21:07:48 +0900

    feat(InputView): 상품명과 수량을 요구하는 메시지 추가

--- src/main/java/store/common/ViewMessage.java
+++ src/main/java/store/common/ViewMessage.java
@@ -2,7 +2,8 @@ package store.common;
 
 public enum ViewMessage {
 
-    STORE_START_MESSAGE("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n\n");
+    STORE_START_MESSAGE("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n\n"),
+    INPUT_PRODUCT_NAME_AND_QUANTITY("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
 
     private final String message;
 
--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -4,17 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 import store.validator.InputViewValidator;
 import store.validator.ParserValidator;
 
-public class InputView {
+import static store.common.ViewMessage.INPUT_PRODUCT_NAME_AND_QUANTITY;
 
-    public static String readProducts() {
-        String userInput = readUserInput();
-        ParserValidator.validateIndividualProductDelimiter(userInput);
-        return userInput;
-    }
+public class InputView {
 
     public static String readUserInput() {
         String userInput = Console.readLine();
         InputViewValidator.validateUserInputIsNotEmpty(userInput);
+        return userInput;
+    }
+
+    public static String readProducts() {
+        System.out.println(INPUT_PRODUCT_NAME_AND_QUANTITY.getMessage());
+        String userInput = readUserInput();
+        ParserValidator.validateIndividualProductDelimiter(userInput);
         return userInput;
     }
 
