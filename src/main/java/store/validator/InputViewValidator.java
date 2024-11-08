b42a4e4c839a05ece72788f1ff986cd9a9dafe5e
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-08 20:53:43 +0900

    refactor(InputViewValidator): 예외 메시지 오타 수정

--- src/main/java/store/validator/InputViewValidator.java
+++ src/main/java/store/validator/InputViewValidator.java
@@ -4,7 +4,7 @@ public class InputViewValidator {
 
     public static void validateUserInputIsNotEmpty(final String userInput) {
         if (userInput == null || userInput.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 상품명과 수량을 비어있을 수 없습니다.");
+            throw new IllegalArgumentException("[ERROR] 상품명과 수량은 비어있을 수 없습니다.");
         }
     }
 
