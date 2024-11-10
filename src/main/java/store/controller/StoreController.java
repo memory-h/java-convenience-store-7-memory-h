0a55b8b69d8adb181b431a946d769922a991e7d0
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-10 23:25:57 +0900

    feat(StoreController): 영수증 생성 및 데이터 계산 로직 추가
    startPurchaseProcess 메서드에 제품 구매 정보와 프로모션 할인을 계산하는 로직 추가
    getProductsPurchase 메서드를 추가하여 사용자 입력으로부터 ProductsPurchase 생성
    receiptPrint 메서드를 추가하여 영수증 출력 로직 구현
    getReceipt 메서드를 통해 총 구매 금액, 프로모션 할인, 멤버십 할인, 최종 결제 금액을 계산

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,10 +1,22 @@
 package store.controller;
 
+import store.domain.PurchaseSummaryList;
+import store.domain.ReceiptResult;
+import store.domain.conveniencestore.Product;
 import store.domain.conveniencestore.Products;
 import store.domain.ProductsPurchase;
 import store.domain.ProductsUserInput;
+import store.domain.conveniencestore.Promotion;
 import store.domain.conveniencestore.Promotions;
+import store.utils.PurchaseSummaryGenerator;
 import store.view.InputView;
+import store.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
+
+import static store.utils.calculator.PromotionCalculator.calculatePromotionPurchase;
+import static store.utils.calculator.PurchaseReceipt.*;
 
 public class StoreController {
 
@@ -15,13 +27,38 @@ public class StoreController {
         final Products products = storeInventoryController.init();
         final Promotions promotions = storePromotionController.init();
 
-        startPurchaseProcess(products, promotions);
+        startPurchaseProcess(products.getProducts(), promotions.getPromotions());
     }
 
-    private void startPurchaseProcess(final Products products, final Promotions promotions) {
+    private void startPurchaseProcess(final List<Product> products, final List<Promotion> promotions) {
+        try {
+            Map<String, Integer> productsPurchase = getProductsPurchase(products).getProductsPurchase();
+            Map<String, Integer> promotionResult = calculatePromotionPurchase(productsPurchase, products, promotions);
+            receiptPrint(products, productsPurchase, promotionResult);
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+            startPurchaseProcess(products, promotions);
+        }
+    }
+
+    private ProductsPurchase getProductsPurchase(final List<Product> products) {
         String userInput = InputView.readProducts();
         ProductsUserInput productsUserInput = ProductsUserInput.from(userInput);
-        ProductsPurchase productsPurchase = ProductsPurchase.of(products.getProducts(), productsUserInput.getProductsUserInput());
+        return ProductsPurchase.of(products, productsUserInput.getProductsUserInput());
+    }
+
+    private void receiptPrint(final List<Product> products, final Map<String, Integer> productsPurchase, final Map<String, Integer> promotionResult) {
+        ReceiptResult receiptResult = getReceipt(products, productsPurchase, promotionResult);
+        PurchaseSummaryList purchaseSummaryList = PurchaseSummaryGenerator.generateSummaryList(productsPurchase, products);
+        OutputView.receiptMessage(receiptResult.getReceiptData(), promotionResult, purchaseSummaryList.getPurchaseSummaries());
+    }
+
+    private ReceiptResult getReceipt(final List<Product> products, final Map<String, Integer> productsPurchase, final Map<String, Integer> promotionResult) {
+        int totalPurchasesReceipt = calculateTotalPurchases(productsPurchase, products);
+        int promotionDiscountReceipt = calculatePromotionDiscount(promotionResult, products);
+        int membershipDiscountReceipt = calculateMembershipDiscount(productsPurchase, products);
+        int receiveToMoneyReceipt = calculateFinalAmount(totalPurchasesReceipt, promotionDiscountReceipt, membershipDiscountReceipt);
+        return ReceiptResult.of(totalPurchasesReceipt, promotionDiscountReceipt, membershipDiscountReceipt, receiveToMoneyReceipt);
     }
 
 }
