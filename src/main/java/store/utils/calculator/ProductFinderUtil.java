97cc022fab95060ef722633fe181eca5c47a6cbc
Hyeon Young Jang <gusdud9124@gmail.com>
2024-11-11 19:13:01 +0900

    refactor(ProductFinderUtil): 매직 넘버를 상수화

--- src/main/java/store/utils/calculator/ProductFinderUtil.java
+++ src/main/java/store/utils/calculator/ProductFinderUtil.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 public class ProductFinderUtil {
 
+    private static final String NOT_PROMOTION = "null";
+
     private ProductFinderUtil() {
     }
 
     public static Product findRegularProductByName(final List<Product> products, final String productName) {
         return products.stream()
-                .filter(product -> product.getName().equals(productName) && "null".equals(product.getPromotion()))
+                .filter(product -> product.getName().equals(productName) && product.getPromotion().equals(NOT_PROMOTION))
                 .findFirst()
                 .orElse(null);
     }
